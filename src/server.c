832bef217e125bc6ea8f71b4adc32da40594f316
gqhao <haoguoqing123@sina.com>
2020-06-04 14:33:06 +0800

    use htp-sentinel instead of redis-sentinel

--- src/server.c
+++ src/server.c
@@ -3541,7 +3541,8 @@ void memtest(size_t megabytes, int passes);
 int checkForSentinelMode(int argc, char **argv) {
     int j;
 
-    if (strstr(argv[0],"redis-sentinel") != NULL) return 1;
+    //    if (strstr(argv[0],"redis-sentinel") != NULL) return 1;
+    if (strstr(argv[0],"htp-sentinel") != NULL) return 1;
     for (j = 1; j < argc; j++)
         if (!strcmp(argv[j],"--sentinel")) return 1;
     return 0;
